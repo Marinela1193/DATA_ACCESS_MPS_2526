d340c976f44181426ed2af616a5075be781dd05d
Marinela1193 <marpersuar2@alu.edu.gva.es>
2026-02-21 11:25:00 +0100

    Revert "añadimos actividad segundo trimestre"
    This reverts commit a1e7dc19186c7212af2d1e38aca37a905fc0df67.

--- FinalActivities/FirstPeriod/src/main/java/org/example/Endpoints.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Endpoints {
-    static final String SERVER = "http://localchost:8080/";
-    static final String STUDENTS = "/students/";
-    static final String API_PATH = "api";
-}
